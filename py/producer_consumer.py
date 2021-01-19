7e6e3f0a952c94247f9b1bd560643ccf8de72996
Santiago Hurtado <shurtado@rosen-group.com>
2021-01-19 12:54:04 +0100

    feat: simple producer consumer with end of queue

--- py/producer_consumer.py
+++ py/producer_consumer.py
@@ -3,7 +3,7 @@ import threading
 import time
 
 serving_line = queue.Queue(maxsize=5)
-
+consumers = 2
 
 def service_producer():
     for i in range(20):
@@ -11,15 +11,20 @@ def service_producer():
         print(f'serve {i} remaining capacity {serving_line.maxsize - serving_line.qsize()}')
         time.sleep(0.2)
 
+    for i in range(consumers):
+        serving_line.put_nowait('eoq')
+
 
 def service_consumer():
     while True:
         item = serving_line.get()
         print(f'get item {item}')
+        if item == 'eoq':
+            break
         time.sleep(0.3)
 
 
 if __name__ == '__main__':
-    for i in range(2):
+    for c in range(consumers):
         threading.Thread(target=service_consumer).start()
     threading.Thread(target=service_producer).start()
