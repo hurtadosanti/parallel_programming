7279b1caa4330323c163f6d04ce549df9fd50b9c
Santiago Hurtado <shurtado@rosen-group.com>
2021-01-27 14:38:13 +0100

    feat: using atomics

---
+++ cpp/atomic_object.cpp
@@ -0,0 +1,22 @@
+//
+// Created by SHurtado on 1/27/2021.
+//
+#include <thread>
+#include <atomic>
+#include <iostream>
+
+std::atomic<unsigned int> count(0);
+
+void counter(){
+    for(unsigned int i=0;i<1000000;i++){
+        count++;
+    }
+}
+
+int main(){
+    std::thread t1(counter);
+    std::thread t2(counter);
+    t1.join():
+    t2.join():
+    std::cout<<"counter:"<<count.load()<<std::endl;
+}
