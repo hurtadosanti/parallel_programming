e95463c039fd6d3194814b066229139eac0e8dc7
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-01-30 18:53:37 +0100

    feat: script to install the benchmark library

---
+++ cpp/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+git clone https://github.com/google/benchmark.git
+git clone https://github.com/google/googletest.git benchmark/googletest
+cd benchmark
+cmake -E make_directory "build" &&
+cmake -DCMAKE_BUILD_TYPE=Release -S . -B "build"
+sudo cmake --build "build" --config Release --target install
