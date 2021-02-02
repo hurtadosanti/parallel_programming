f5adc106e27b3aefd0121148f8dd4a8a854fc8ee
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-02-02 11:24:19 +0100

    feat: run in parallel

--- cpp/samples/parallel_math.cpp
+++ cpp/samples/parallel_math.cpp
@@ -21,6 +21,10 @@ bool Samples::ParallelMath::is_prime(int number) {
 
 double Samples::ParallelMath::calculate_sum_primes(const size_t size) {
     auto result = 0.0;
+    size_t split = 6;
+    int partitions = size/split;
+    int remainder = size%split;
+    int k = (size%split == 0)? size : split-size%split;
     for(size_t i=0;i<size;++i){
         if(is_prime(i)){
             result+=i;
---
+++ py/multiprocess.py
@@ -0,0 +1,4 @@
+size=100
+split=2
+res = size/split
+split - size % split
