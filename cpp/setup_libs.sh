9fc438e510579a1420ada74bba0f4191be099f1e
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-02-05 11:35:46 +0100

    chore: Build, Tests and Installation

--- cpp/setup_libs.sh
+++ cpp/setup_libs.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-sudo apt install -yq cmake make gcc g++ valgrind linux-tools-virtual linux-tools-generic linux-tools-$(uname -r)
 git clone https://github.com/google/benchmark.git
 git clone https://github.com/google/googletest.git benchmark/googletest
 cd benchmark
 cmake -E make_directory "build"
 cmake -DCMAKE_BUILD_TYPE=Release -S . -B "build"
-sudo cmake --build "build" --config Release --target install
+cmake --build "build" --config Release --target install
 cd googletest
 cmake -E make_directory "build"
 cmake -DCMAKE_BUILD_TYPE=Release -S . -B "build"
-sudo cmake --build "build" --confstart.ig Release --target install
+cmake --build "build" --config Release --target install
