6475f7d719dc3d91ce056b48f9dc16582561fa95
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-02-16 10:39:49 +0100

    chore: move out code into a self project

--- cpp/setup_perf.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Kernel setup to allow the perf usage
-sudo apt-get install -yqq valgrind linux-tools-virtual linux-tools-generic linux-tools-$(uname -r)
-sudo sh -c 'echo 0 > /proc/sys/kernel/perf_event_paranoid'
-sudo sh -c 'echo 0 > /proc/sys/kernel/kptr_restrict'
--- cpp/tests/test_recipes.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by shurtado on 09.02.21.
-//
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
